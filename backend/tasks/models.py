52569dffd1eca926e633b6d0c8b1514ef5e28395
Solaiman Hossain <slmnsnto@gmail.com>
2025-07-15 19:06:55 +0600

    feat(tasks): add Task model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Task(models.Model):
+    STATUS = (
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed')
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=50, choices=STATUS, default='pending')
+    due_date = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['due_date']
