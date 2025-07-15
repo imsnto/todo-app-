543994148247d64aa337a0cb15e19da7d102ccbe
Solaiman Hossain <slmnsnto@gmail.com>
2025-07-15 18:56:58 +0600

    feat(auth): implement user registrations and login functionality
    - created login and register view logic
    - extended jwt access and refresh token's lifetime
    - got access and refresh token when an user logged in

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
+
+
     'accounts',
     'tasks',
 ]
@@ -124,3 +127,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7)
+}
